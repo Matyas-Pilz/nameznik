c772c63136bb66f9834d4bfa542225ba79b50318
B. Stack <bgstack15@gmail.com>
2025-08-25 08:24:42 -0400

    move recipes to separate file

--- crafting.lua
+++ crafting.lua
@@ -1,1 +1,77 @@
 --crafting
+has_xcompat = minetest.get_modpath("xcompat") ~= nil
+has_mcl_core = minetest.get_modpath("mcl_core") ~= nil
+has_default = minetest.get_modpath("default") ~= nil
+
+-- default ingredients
+local recipes_table = {
+	-- empty simply is left undefined, so nothing is added
+	_r05_ = "default:gravel",
+	_r1_ = "default:sand",
+	_r05h_ = "default:sandstone",
+	_r1h_ = "default:flint"
+}
+local dye_red = "dye:red"
+local dye_white = "dye:white"
+local steel_ingot = "default:steel_ingot"
+local cobble = "default:cobble"
+local base_ingredient = "default:copper_ingot"
+if has_xcompat then
+	recipes_table = {
+	  _r05_ = xcompat.materials["gravel"],
+	  _r1_ = xcompat.materials["sand"],
+	  _r05h_ = xcompat.materials["sandstone"],
+	  _r1h_ = xcompat.materials["flint"]
+	}
+	dye_red = xcompat.materials["dye_red"]
+	dye_white = xcompat.materials["dye_white"]
+	steel_ingot = xcompat.materials["steel_ingot"]
+	cobble = xcompat.materials["cobble"]
+	base_ingredient = xcompat.materials["copper_ingot"]
+elseif has_mcl_core then
+	recipes_table = {
+	  _r05_ = "mcl_core:gravel",
+	  _r1_ = "mcl_core:sand",
+	  _r05h_ = "mcl_core:sandstone",
+	  _r1h_ = "mcl_core:flint"
+	}
+	dye_red = "mcl_colorblocks:concrete_red"
+	dye_white = "mcl_colorblocks:concrete_white"
+	steel_ingot = "mcl_core:iron_ingot"
+	cobble = "mcl_core:cobble"
+	base_ingredient = "mcl_copper:copper_ingot"
+end
+-- We still want to use concrete specifically if we have mcl_core
+if has_mcl_core then
+	dye_red = "mcl_colorblocks:concrete_red"
+	dye_white = "mcl_colorblocks:concrete_white"
+end
+
+function nameznik.register_recipe(nname,nnum,count,_type)
+	local this_recipe = {dye_white}
+	-- add red color
+	if nname == "koncovnik_" or nname == "nameznik_konc_" or nname == "koncovnik_konc_" then
+		table.insert(this_recipe,dye_red)
+	end
+	if nname == "koncovnik_" then
+		table.insert(this_recipe,steel_ingot)
+	end
+	if nname == "hranicnik" then
+		table.insert(this_recipe,cobble)
+	end
+	if recipes_table[_type] ~= nil then
+		table.insert(this_recipe, recipes_table[_type])
+	end
+	local x = tonumber(nnum)
+	if x == nil then x = 0 end
+	for _ = 1, x do
+		table.insert(this_recipe,base_ingredient)
+	end
+	_type = _type:sub(2)
+	--if _type ~= nil and _type ~= "" then _type = _type .. "_" end
+	minetest.register_craft({
+		output = "nameznik:" .. _type .. nname .. nnum .. " " .. count,
+		type = "shapeless",
+		recipe = this_recipe
+	})
+end
--- nameznik.lua
+++ nameznik.lua
@@ -1,7 +1,4 @@
 local S = core.get_translator("nameznik")
-has_xcompat = minetest.get_modpath("xcompat") ~= nil
-has_mcl_core = minetest.get_modpath("mcl_core") ~= nil
-has_default = minetest.get_modpath("default") ~= nil
 
 local nameznik__groups = {dig_immediate=2, not_blocking_trains=1}
 local nameznik__simplenodebox = {type = "fixed",fixed = {
@@ -43,79 +40,6 @@ local function get_palette(nname)
 	return "nameznik_palette1.png"
 end
 
--- default ingredients
-local recipes_table = {
-	-- empty simply is left undefined, so nothing is added
-	_r05_ = "default:gravel",
-	_r1_ = "default:sand",
-	_r05h_ = "default:sandstone",
-	_r1h_ = "default:flint"
-}
-local dye_red = "dye:red"
-local dye_white = "dye:white"
-local steel_ingot = "default:steel_ingot"
-local cobble = "default:cobble"
-local base_ingredient = "default:copper_ingot"
-if has_xcompat then
-	recipes_table = {
-	  _r05_ = xcompat.materials["gravel"],
-	  _r1_ = xcompat.materials["sand"],
-	  _r05h_ = xcompat.materials["sandstone"],
-	  _r1h_ = xcompat.materials["flint"]
-	}
-	dye_red = xcompat.materials["dye_red"]
-	dye_white = xcompat.materials["dye_white"]
-	steel_ingot = xcompat.materials["steel_ingot"]
-	cobble = xcompat.materials["cobble"]
-	base_ingredient = xcompat.materials["copper_ingot"]
-elseif has_mcl_core then
-	recipes_table = {
-	  _r05_ = "mcl_core:gravel",
-	  _r1_ = "mcl_core:sand",
-	  _r05h_ = "mcl_core:sandstone",
-	  _r1h_ = "mcl_core:flint"
-	}
-	dye_red = "mcl_colorblocks:concrete_red"
-	dye_white = "mcl_colorblocks:concrete_white"
-	steel_ingot = "mcl_core:iron_ingot"
-	cobble = "mcl_core:cobble"
-	base_ingredient = "mcl_core:copper"
-end
--- We still want to use concrete specifically if we have mcl_core
-if has_mcl_core then
-	dye_red = "mcl_colorblocks:concrete_red"
-	dye_white = "mcl_colorblocks:concrete_white"
-end
-
-local function register_recipe(nname,nnum,count,_type)
-	local this_recipe = {dye_white}
-	-- add red color
-	if nname == "koncovnik_" or nname == "nameznik_konc_" or nname == "koncovnik_konc_" then
-		table.insert(this_recipe,dye_red)
-	end
-	if nname == "koncovnik_" then
-		table.insert(this_recipe,steel_ingot)
-	end
-	if nname == "hranicnik" then
-		table.insert(this_recipe,cobble)
-	end
-	if recipes_table[_type] ~= nil then
-		table.insert(this_recipe, recipes_table[_type])
-	end
-	local x = tonumber(nnum)
-	if x == nil then x = 0 end
-	for _ = 1, x do
-		table.insert(this_recipe,base_ingredient)
-	end
-	_type = _type:sub(2)
-	--if _type ~= nil and _type ~= "" then _type = _type .. "_" end
-	minetest.register_craft({
-		output = "nameznik:" .. _type .. nname .. nnum .. " " .. count,
-		type = "shapeless",
-		recipe = this_recipe
-	})
-end
-
 local function nameznik_regnode(nname, nnum, ndesc, nfront, nside, nback)
 	local npalette = get_palette(nname)
 
@@ -138,7 +62,7 @@ local function nameznik_regnode(nname, nnum, ndesc, nfront, nside, nback)
 		is_ground_content = false,
 		palette = npalette,
 	})
-	register_recipe(nname, nnum, 15, "")
+	nameznik.register_recipe(nname, nnum, 15, "")
 end
 
 local function nameznik_regnoder05(nname, nnum, ndesc, nfront, nside, nback)
@@ -164,7 +88,7 @@ local function nameznik_regnoder05(nname, nnum, ndesc, nfront, nside, nback)
 		is_ground_content = false,
 		palette = npalette,
 	})
-	register_recipe(nname, nnum, 15, "_r05_")
+	nameznik.register_recipe(nname, nnum, 15, "_r05_")
 end
 
 local function nameznik_regnoder1(nname, nnum, ndesc, nfront, nside, nback)
@@ -190,7 +114,7 @@ local function nameznik_regnoder1(nname, nnum, ndesc, nfront, nside, nback)
 		is_ground_content = false,
 		palette = npalette,
 	})
-	register_recipe(nname, nnum, 15, "_r1_")
+	nameznik.register_recipe(nname, nnum, 15, "_r1_")
 end
 
 local function nameznik_regnoder05h(nname, nnum, ndesc, nfront, nside, nback)
@@ -216,7 +140,7 @@ local function nameznik_regnoder05h(nname, nnum, ndesc, nfront, nside, nback)
 		is_ground_content = false,
 		palette = npalette,
 	})
-	register_recipe(nname, nnum, 15, "_r05h_")
+	nameznik.register_recipe(nname, nnum, 15, "_r05h_")
 end
 
 local function nameznik_regnoder1h(nname, nnum, ndesc, nfront, nside, nback)
@@ -242,7 +166,7 @@ local function nameznik_regnoder1h(nname, nnum, ndesc, nfront, nside, nback)
 		is_ground_content = false,
 		palette = npalette,
 	})
-	register_recipe(nname, nnum, 15, "_r1h_")
+	nameznik.register_recipe(nname, nnum, 15, "_r1h_")
 end
 
 nameznik_regnode("nameznik_","1","Fouling point marker","nameznik_over1.png","","nameznik_over1.png")
